757c839c7d1272d87bcbf916840cfddd299e4312
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-04 22:05:47 -0300

    feat: alert component

---
+++ src/core/components/alert/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+const Alert = () => {
+    return (
+        <Container>
+            Ganhe R$ 10,00 de desconto no frete
+        </Container>
+    )
+}
+
+export default Alert;
