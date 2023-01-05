55b732731ab6d456a6da23efc45dc1e4b77963b9
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-04 22:07:06 -0300

    feat: use home on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './modules/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>App</p>
-      </header>
+      <Home/>
     </div>
   );
 }
