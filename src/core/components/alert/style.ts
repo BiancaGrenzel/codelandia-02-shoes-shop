20051b8d3ea5413f5790c8357c30ebc4ac632736
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-04 22:05:53 -0300

    feat: alert style

---
+++ src/core/components/alert/style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+    background-color: #181818;
+    color: #fff;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    width: 100vw;
+    height: auto;
+`
