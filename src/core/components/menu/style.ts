54d4ee00bfa341af966f9545dd82c2520780a6b1
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-06 22:57:58 -0300

    feat: menu style

---
+++ src/core/components/menu/style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+    background-color: #fff;
+    color: #181818;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 29px;
+    width: calc(100vw - 32px);
+    height: auto;
+`
