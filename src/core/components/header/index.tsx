6e8f4912fcfa53150bc97ed2c50331e480845db9
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-06 22:57:32 -0300

    feat: header component

---
+++ src/core/components/header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Mask, Subtitle, Title } from "./style";
+
+const Header = () => {
+    return(
+        <Container>
+            <Mask>
+                <Title>A melhor loja de Jordan</Title>
+                <Subtitle>O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.</Subtitle>
+            </Mask>
+        </Container> 
+    )
+}
+
+export default Header;
