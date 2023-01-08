e7e90898a54070296fc35179a42de45fa3e23c5a
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-07 21:37:49 -0300

    feat: shoes json mock

---
+++ src/core/json/shoes.ts
@@ -0,0 +1,42 @@
+export const Shoes = [
+    {
+        "id": 1,
+        "image": require("../assets/jordans/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.svg").default,
+        "name":"Air Jordan 1 High Zoom CMFT Tropical Twist",
+    },
+    {
+        "id": 2,
+        "image": require("../assets/jordans/air-jordan-1-mid-black-noble-red-w-1-1000.svg").default,
+        "name": "Air Jordan 1 Mid Black Noble Red W"
+    },
+    {
+        "id": 3,
+        "image": require("../assets/jordans/air-jordan-1-mid-bright-citrus-1-1000 1.svg").default,
+        "name": "Air Jordan 1 Mid Bright Citrus"
+    },
+    {
+        "id": 4,
+        "image": require("../assets/jordans/air-jordan-1-mid-carbon-fiber-1-1000 1.svg").default,
+        "name": "Air Jordan 1 Mid Carbon Fiber"
+    },
+    {
+        "id": 5,
+        "image": require("../assets/jordans/air-jordan-1-mid-dutch-green-1-400.svg").default,
+        "name": "Air Jordan 1 Mid Dutch Green"
+    },
+    {
+        "id": 6,
+        "image": require("../assets/jordans/air-jordan-1-mid-grey-camo-1-1000 1.svg").default,
+        "name": "Air Jordan 1 Mid Grey Camo"
+    },
+    {
+        "id": 7,
+        "image": require("../assets/jordans/air-jordan-1-mid-light-smoke-grey-gs-1-1000.svg").default,
+        "name": "Air Jordan 1 Mid Light Smoke Grey GS"
+    },
+    {
+        "id": 8,
+        "image": require("../assets/jordans/air-jordan-1-retro-high-court-purple-w-1-400.svg").default,
+        "name": "Air Jordan 1 Retro High Court Purple W"
+    }
+]
