970521b1f5d596b92c83e6bb05021ac9fcc0ef4b
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-07 21:39:16 -0300

    feat: list cards style on home

--- src/modules/home/style.ts
+++ src/modules/home/style.ts
@@ -3,10 +3,19 @@ import styled from "@emotion/styled";
 export const Section = styled.div`
     display: flex;
     flex-direction: column;
-    overflow: auto;
 `
 
 export const Container = styled(Section)`
     justify-content: space-between;
     height: 100vh;
+    max-height: 100vh;
+`
+
+export const ListCards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 4% 4%;
+    gap: 20px;
+    overflow: auto;
+    justify-content: center;
 `
