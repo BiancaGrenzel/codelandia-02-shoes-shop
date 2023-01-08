1e5dc60b0c293bcdbcee3d32e2792a7a3a8b8a92
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-07 21:38:05 -0300

    feat: card component style

---
+++ src/core/components/card/style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color:#EBE9EA;
+    padding: 76px 82px;
+`
